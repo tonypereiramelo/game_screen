3538a5ce0c732cdd3b7abd73e25a075e9c255f53
Tony Melo <tonypereiramelo@gmail.com>
2022-12-07 18:35:00 -0300

    feat(game_screen): :construction: change shottile

--- lib/game_screen/presentation/widgets/shot_tile.dart
+++ lib/game_screen/presentation/widgets/shot_tile.dart
@@ -7,25 +7,27 @@ class ShotTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TimelineTile(
-      indicatorStyle: const IndicatorStyle(width: 16),
+      indicatorStyle: const IndicatorStyle(
+        width: 40,
+        height: 40,
+        indicator: CircleAvatar(
+          child: Icon(Icons.person),
+        ),
+      ),
       alignment: TimelineAlign.start,
       endChild: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Row(
-              children: const [
-                CircleAvatar(
-                  child: Icon(Icons.person),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(32, 0, 0, 0),
-                  child: Text('Gol'),
-                ),
-              ],
+            const Padding(
+              padding: EdgeInsets.fromLTRB(32, 0, 0, 0),
+              child: Text('Goal'),
             ),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.play_circle))
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.play_circle),
+            )
           ],
         ),
       ),
